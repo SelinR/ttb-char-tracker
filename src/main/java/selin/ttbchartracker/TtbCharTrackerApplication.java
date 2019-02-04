59e0c5bbbcd2533f256459db9998beaaf4ef2881
Tifider <Tifider@gmail.com>
2019-02-04 20:21:55 +0300

    First commit

---
+++ src/main/java/selin/ttbchartracker/TtbCharTrackerApplication.java
@@ -0,0 +1,14 @@
+package selin.ttbchartracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtbCharTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtbCharTrackerApplication.class, args);
+	}
+
+}
+
